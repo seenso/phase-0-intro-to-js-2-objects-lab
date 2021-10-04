cdcc37e98f5e981ed5a97f67c5646e4875c0b76e
seenso <>
2021-10-04 15:38:13 -0800

    Passed test for updateEmployeeWithKeyAndValue(employee, key, value)

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Write your solution in this file!
 let employee = {name: "Seen", streetAddress: "123 Snowflake Lane"};
 
-function updateEmployeeWithKeyAndValue(obj, key, value) {}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    let newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
 
-function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
 
-function deleteFromEmployeeByKey(obj, key) {}
+function deleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
 
 function destructivelyDeleteFromEmployeeByKey(obj, key) {}
